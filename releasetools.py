54453cf85c3eabca1e4101da9b3d329bbc9eb2ed
udhaya68-ai <62876759+udhaya68-ai@users.noreply.github.com>
2020-04-24 04:46:01 +0530

    Update releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -51,9 +51,9 @@ def AddImage(info, input_zip, basename, dest):
 def OTA_InstallEnd(info, input_zip):
   AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  return
 
 
-return
 
 def AddBasebandAssertion(info, input_zip):
   android_info = input_zip.read("OTA/android-info.txt")
