1e3b2831ced07d2942f00718365302a2bc704172
udhaya68-ai <62876759+udhaya68-ai@users.noreply.github.com>
2020-04-25 11:13:39 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_violet.mk
+    $(LOCAL_DIR)/carbon_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_violet-userdebug
+    carbon_violet-userdebug
